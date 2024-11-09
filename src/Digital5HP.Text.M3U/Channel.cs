70b308e3653e51d203821705e992323518876f5e
Shay Nissel <shay@nissel.me>
2024-11-09 14:21:24 -0800

    add M3U support for channel-id, channel-number and tvg-chno

--- src/Digital5HP.Text.M3U/Channel.cs
+++ src/Digital5HP.Text.M3U/Channel.cs
@@ -10,6 +10,8 @@ public class Channel
 
     public string TvgName { get; set; }
 
+    public int? TvgChannelNumber { get; set; }
+
 #pragma warning disable CA1056 // URI-like properties should not be strings
     public string LogoUrl { get; set; }
 #pragma warning restore CA1056 // URI-like properties should not be strings
@@ -19,4 +21,8 @@ public class Channel
     public int Duration { get; set; } = -1;
 
     public string Title { get; set; }
+
+    public string ChannelId { get; set; }
+
+    public int? ChannelNumber { get; set; }
 }
